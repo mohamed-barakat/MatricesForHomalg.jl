84a648456a79d4c59fb9929e4d7cc1d98fdf9ec7
Johanna Knecht <johanna.knecht@student.uni-siegen.de>
2023-08-10 09:47:33 +0200

    Implemented ZeroRows/Columns

--- src/MatricesForHomalg.jl
+++ src/MatricesForHomalg.jl
@@ -469,7 +469,84 @@ function BasisOfColumns(mat)::TypeOfMatrixForHomalg
     return TransposedMatrix(BasisOfRows(TransposedMatrix(mat)))
 end
 
-export HomalgRing, NumberRows, NumberColumns, TransposedMatrix, ConvertMatrixToRow, ConvertMatrixToColumn, RowReducedEchelonForm, BasisOfRows, BasisOfColumns
+"""
+    ZeroRows(mat)
+
+Return a (possibly empty) list of positive integers. The list of zero rows of the matrix A.
+
+```jldoctest
+julia> mat = HomalgMatrix(4:9, 3, 2, ZZ)
+[4   5]
+[6   7]
+[8   9]
+
+julia> ZeroRows(mat)
+Int64[]
+
+julia> mat = HomalgMatrix([0, 2, 6, 0, 0, 0], 3, 2, ZZ)
+[0   2]
+[6   0]
+[0   0]
+
+julia> ZeroRows(mat)
+1-element Vector{Int64}:
+ 3
+
+julia> mat = HomalgZeroMatrix(3,3,ZZ)
+[0   0   0]
+[0   0   0]
+[0   0   0]
+
+julia> ZeroRows(mat)
+3-element Vector{Int64}:
+ 1
+ 2
+ 3
+```
+"""
+function ZeroRows(mat)::Vector{Int64}
+    return filter(i -> IsZero(CertainRows(mat, [i])), 1:NumberRows(mat))
+end
+
+"""
+    ZeroColumns(mat)
+
+Return a (possibly empty) list of positive integers. The list of zero columns of the matrix A.
+
+```jldoctest
+julia> mat = HomalgMatrix(4:9, 2, 3, ZZ)
+[4   5   6]
+[7   8   9]
+
+julia> ZeroColumns(mat)
+Int64[]
+
+julia> mat = HomalgMatrix([0, 2, 6, 0, 0, 0], 2, 3, ZZ)
+[0   2   6]
+[0   0   0]
+
+julia> ZeroColumns(mat)
+1-element Vector{Int64}:
+ 1
+
+julia> mat = HomalgZeroMatrix(3,3,ZZ)
+[0   0   0]
+[0   0   0]
+[0   0   0]
+
+julia> ZeroColumns(mat)
+3-element Vector{Int64}:
+ 1
+ 2
+ 3
+```
+"""
+function ZeroColumns(mat)::Vector{Int64}
+    return filter(i -> IsZero(CertainColumns(mat, [i])), 1:NumberColumns(mat))
+end
+
+export HomalgRing, NumberRows, NumberColumns, TransposedMatrix, ConvertMatrixToRow, ConvertMatrixToColumn,
+    RowReducedEchelonForm, BasisOfRows, BasisOfColumns, ZeroRows, ZeroColumns
 
 ## Operations of homalg matrices
 
