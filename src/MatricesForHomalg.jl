28d4981c3dab035f2ca7b1b8a917bc6f08bab97b
Johanna Knecht <johanna.knecht@student.uni-siegen.de>
2023-08-29 10:41:10 +0200

    HomalgMatrix now accepts lists of lists

--- src/MatricesForHomalg.jl
+++ src/MatricesForHomalg.jl
@@ -83,13 +83,20 @@ TypeOfMatrixForHomalg = AbstractAlgebra.Generic.MatSpaceElem
 Construct a (r x c)-matrix over the ring R with L as list of entries
 
 ```jldoctest
-julia> mat = HomalgMatrix([1,2,3,4,5,6], 2, 3, ZZ)
+julia> mat1 = HomalgMatrix([1,2,3,4,5,6], 2, 3, ZZ)
 [1   2   3]
 [4   5   6]
+
+julia> mat2 = HomalgMatrix([[1,2,3],[4,5,6]], 2, 3, ZZ)
+[1   2   3]
+[4   5   6]
+
+julia> mat1 == mat2
+true
 ```
 """
 function HomalgMatrix(entries, r, c, R)::TypeOfMatrixForHomalg
-    return matrix(R, r, c, entries)
+    return matrix(R, r, c, vcat(entries...))
 end
 
 """
